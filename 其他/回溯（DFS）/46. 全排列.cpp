eeae38845cf3c5d6e0fbaea245633a5d0d8bd34e
zjdgit <973687433@qq.com>
2021-01-21 08:12:34 +0800

    Add files via upload

---
+++ 其他/回溯（DFS）/46. 全排列.cpp
@@ -0,0 +1,44 @@
+/*
+给定一个 没有重复 数字的序列，返回其所有可能的全排列。
+
+示例:
+
+输入: [1,2,3]
+输出:
+[
+  [1,2,3],
+  [1,3,2],
+  [2,1,3],
+  [2,3,1],
+  [3,1,2],
+  [3,2,1]
+]
+*/
+
+class Solution {
+public:
+	vector<vector<int>> permute(vector<int>& nums) {
+		vector<vector<int>> res;
+		vector<int> track;
+		backtrack(res, nums, track);
+		return res;
+	}
+	void backtrack(vector<vector<int>>& res, vector<int> nums, vector<int>& track)
+	{
+		if (track.size() == nums.size())
+		{
+			res.push_back(track);
+			return;
+		}
+		for (int i = 0; i < nums.size(); i++)
+		{
+			if (std::find(track.begin(), track.end(), nums[i]) != track.end())
+			{
+				continue;
+			}
+			track.push_back(nums[i]);
+			backtrack(res, nums, track);
+			track.pop_back();
+		}
+	}
+};
