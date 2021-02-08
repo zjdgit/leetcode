f2e768ffe4fafb62dd0858c0fea525c889ec671c
zjdgit <973687433@qq.com>
2021-02-09 00:12:41 +0800

    滑动窗口算法

---
+++ 其他/438. 找到字符串中所有字母异位词.cpp
@@ -0,0 +1,68 @@
+﻿/*
+给定一个字符串 s 和一个非空字符串 p，找到 s 中所有是 p 的字母异位词的子串，返回这些子串的起始索引。
+
+字符串只包含小写英文字母，并且字符串 s 和 p 的长度都不超过 20100。
+
+说明：
+
+字母异位词指字母相同，但排列不同的字符串。
+不考虑答案输出的顺序。
+示例 1:
+
+输入:
+s: "cbaebabacd" p: "abc"
+
+输出:
+[0, 6]
+
+解释:
+起始索引等于 0 的子串是 "cba", 它是 "abc" 的字母异位词。
+起始索引等于 6 的子串是 "bac", 它是 "abc" 的字母异位词。
+*/
+
+class Solution {
+public:
+	vector<int> findAnagrams(string s, string p) {
+		unordered_map<char, int> need, window;
+		for (char c : p)
+		{
+			need[c]++;
+		}
+		int left = 0, right = 0;
+		int valid = 0;
+		vector<int> res;
+		while (right < s.size())
+		{
+			char c = s[right];
+			right++;
+			if (need.count(c))
+			{
+				window[c]++;
+				if (need[c] == window[c])
+				{
+					valid++;
+				}
+			}
+
+			while (right - left == p.size())
+			{
+				if (valid == need.size())
+				{
+					res.push_back(left);
+				}
+
+				char d = s[left];
+				left++;
+				if (need.count(d))
+				{
+					if (need[d] == window[d])
+					{
+						valid--;
+					}
+					window[d]--;
+				}
+			}
+		}
+		return res;
+	}
+};
