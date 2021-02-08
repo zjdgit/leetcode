9dfb1e4a161f106ad66962fb113c9b0f8c665623
zjdgit <973687433@qq.com>
2021-02-08 23:45:52 +0800

    使用滑动窗口算法

---
+++ 其他/76. 最小覆盖子串.cpp
@@ -0,0 +1,67 @@
+﻿/*
+给你一个字符串 s 、一个字符串 t 。返回 s 中涵盖 t 所有字符的最小子串。如果 s 中不存在涵盖 t 所有字符的子串，则返回空字符串 "" 。
+
+注意：如果 s 中存在这样的子串，我们保证它是唯一的答案。
+
+ 
+
+示例 1：
+
+输入：s = "ADOBECODEBANC", t = "ABC"
+输出："BANC"
+示例 2：
+
+输入：s = "a", t = "a"
+输出："a"
+
+*/
+
+class Solution {
+public:
+	string minWindow(string s, string t) {
+		unordered_map<char, int> need, window;
+		for (char c : t)
+		{
+			need[c]++;
+		}
+
+		int left = 0, right = 0;
+		int valid = 0;
+
+		int start = 0, len = INT_MAX;
+		while (right < s.size())
+		{
+			char c = s[right];
+			right++;
+			if (need.count(c))
+			{
+				window[c]++;
+				if (window[c] == need[c])
+				{
+					valid++;
+				}
+			}
+
+			while (valid == need.size())
+			{
+				if (right - left < len)
+				{
+					len = right - left;
+					start = left;
+				}
+
+				char d = s[left];
+				left++;
+				if (need.count(d))
+				{
+					if (need[d] == window[d])
+					{
+						valid--;
+					}
+					window[d]--;
+				}
+			}
+		}
+		return len == INT_MAX ? "" : s.substr(start, len);
+	}
+};
