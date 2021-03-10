3378116aaeb94a833d496a28482aa566c3003d62
zjdgit <973687433@qq.com>
2021-03-11 07:41:46 +0800

    Add files via upload

---
+++ 二叉树/114. 二叉树展开为链表.cpp
@@ -0,0 +1,43 @@
+/*
+给你二叉树的根结点 root ，请你将它展开为一个单链表：
+
+展开后的单链表应该同样使用 TreeNode ，其中 right 子指针指向链表中下一个结点，而左子指针始终为 null 。
+展开后的单链表应该与二叉树 先序遍历 顺序相同。
+
+*/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+	void flatten(TreeNode* root) {
+		if (root == nullptr)
+			return;
+
+		flatten(root->left);
+		flatten(root->right);
+
+		TreeNode* left = root->left;
+		if (left == nullptr)
+			return;
+		TreeNode* right = root->right;
+		root->left = nullptr;
+		root->right = left;
+
+		TreeNode* pNode = left;
+		while (pNode->right)
+		{
+			pNode = pNode->right;
+		}
+		pNode->right = right;
+	}
+};
